bcd0d9517a824aefec3053bd1c7235490f9c8ae8
RovingCondor <108630123+rovingCondor@users.noreply.github.com>
2025-02-17 17:18:53 -0500

    initial

---
+++ reactNativeUI/App.tsx
@@ -0,0 +1,15 @@
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCards from './Components/FlatCards'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
